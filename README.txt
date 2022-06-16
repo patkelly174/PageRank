3c2c1c735dc748eca1ff5432c47494e49bf48b8e
patkelly174 <patkelly174@gmail.com>
2022-06-16 13:48:02 -0400

    Files

---
+++ README.txt
@@ -0,0 +1,3 @@
+The only thing you must download to run this code is the latest version of
+Python. You then need to download the links.srt.gz file from moodle and add it into the src directory.
+Then open a terminal window in the src directory and run the program using python3 evaluator.py
---
+++ src/pagerank.py
@@ -0,0 +1,67 @@
+pages_count = 0
+inlink_count = {}
+graph = {}
+i = {}
+r = {}
+lamda = 0.15
+tau = 0.0001
+
+def convergence(r, i):
+    norm = 0
+    for page in r:
+        total += abs(r[page] - i[page])
+    return norm
+
+with open("links.srt", "r") as f:
+    for line in f:
+        words = line.split()
+        source = words[0]
+        target = words[1]
+        if not target in inlink_count:
+            inlink_count[target] = 1
+        else:
+            inlink_count[target] += 1
+        if source in graph:
+            graph[source].append(target)
+        else:
+            graph[source] = [target]
+        if target not in graph:
+            graph[target] = []
+        pages_count += 1
+        r[source] = None
+        r[target] = None
+        i[source] = None
+        i[target] = None
+    f.close()
+
+############## PAGE RANK ALGO #################
+r = dict.fromkeys(r, lamda / pages_count)
+i = dict.fromkeys(i, 1 / pages_count)
+while not convergence(r, i) < tau:
+    r = dict.fromkeys(r, lamda / pages_count)
+    for page in graph:
+        q = graph[page]
+        accumulator = {}
+        if len(q) > 0:
+            for q_page in q:
+                r[q_page] = r[q_page] + ((1 - lamda) * i[page]) / abs(len(q))
+        else:
+            accumulator[page] = ((1 - lamda) * i[page]) / abs(pages_count)
+    for page in accumulator:
+        r[page] += accumulator[page]
+    i = r
+############## END PAGE RANK ALGO #################
+
+with open("pagerank.txt", "w") as f:
+    sorted_pagerank = sorted(r, key = r.get, reverse = True)[:75]
+    count = 1
+    for pagerank in sorted_pagerank:
+        f.write(str(count) + " " + pagerank + " " + str(r[pagerank]) + "\n")
+        count += 1
+
+with open("inlinks.txt", "w") as f:
+    sorted_inlink = sorted(inlink_count, key = inlink_count.get, reverse = True)[:75]
+    count = 1
+    for inlink in sorted_inlink:
+        f.write(str(count) + " " + inlink + " " + str(inlink_count[inlink]) + "\n")
+        count += 1
